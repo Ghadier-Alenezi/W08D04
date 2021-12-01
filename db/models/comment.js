d6e5648732065a62aef66077314d8cc4f5669ab1
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-01 12:21:22 +0300

    fix like, post, comment schemas

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-const comment = new mongoose.Schema(
-  {
-    desc: { type: String },
-    user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    post: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
-  },
-  { timestamps: true }
-);
+const comment = new mongoose.Schema({
+  desc: { type: String },
+  user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  post: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+});
 
 module.exports = mongoose.model("Comment", comment);
--- db/models/post.js
+++ db/models/post.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const post = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: { type: String },
     desc: { type: String, required: true },
     img: { type: String },
+    createdAt: {type: Date, default: Date.now()},
+    isDel: { type: Boolean, default: false },
     user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  },
-  { timestamps: true }
+  }
 );
 
 module.exports = mongoose.model("Post", post);
